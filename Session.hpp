9be019a49c6fc79ea1e18aad2f75db16235d587a
XNefertar <xnefertar@gmail.com>
2024-10-31 10:19:02 +0800

    Session_and_Manage_Update

---
+++ Session.hpp
@@ -0,0 +1,69 @@
+#ifndef _SESSION_HPP_
+#define _SESSION_HPP_
+
+
+#include <cstdint>
+#include <unordered_map>
+#include <mutex>
+#include <string>
+#include <iostream>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include "Log.hpp"
+
+using namespace LOG_MSG;
+typedef websocketpp::server<websocketpp::config::asio> wsserver_t;
+typedef enum
+{
+    UNLOGIN,
+    LOGIN,
+}SESSION_STATE;
+
+class Session
+{
+private:
+    uint64_t                _UID;
+    uint64_t                _SessionID;
+    SESSION_STATE           _State;
+    wsserver_t::timer_ptr   _TimerPtr;
+
+public:
+    Session(uint64_t uid, uint64_t sid)
+        : _UID(uid),
+          _SessionID(sid),
+          _State(UNLOGIN),
+          _TimerPtr(nullptr)
+    {
+        Log::LogInit("log.txt", NORMAL);
+        Log::LogMessage(INFO, "Session init success");
+    }
+
+    ~Session()
+    {
+        if(_TimerPtr != nullptr)
+        {
+            _TimerPtr->cancel();
+            Log::LogMessage(INFO, "Session destroy");
+        }
+    }
+
+    uint64_t GetUID() { return _UID; }
+    uint64_t GetSessionID() { return _SessionID; }
+    SESSION_STATE GetState() { return _State; }
+    void SetState(SESSION_STATE state) { _State = state; }
+    void SetTimer(const wsserver_t::timer_ptr timer) { _TimerPtr = timer; }
+    void SetUID(uint64_t uid) { _UID = uid; }
+    bool IsLogIn() { return _State == LOGIN; }
+    wsserver_t::timer_ptr& GetTimer() { return _TimerPtr; }
+
+    void CancelTimer()
+    {
+        if (_TimerPtr != nullptr)
+        {
+            _TimerPtr->cancel();
+        }
+    }
+};
+
+
+#endif // _SESSION_HPP_
---
+++ SessionManage.hpp
@@ -0,0 +1,128 @@
+#ifndef _SESSION_MANAGE_HPP_
+#define _SESSION_MANAGE_HPP_
+
+#include "Session.hpp"
+#include "Log.hpp"
+#include <unordered_map>
+
+#define SESSION_TIMEOUT 15000
+#define SESSION_FOREVER -1
+
+using SessionPtr = std::shared_ptr<Session>;
+using namespace LOG_MSG;
+
+class SessionManage
+{
+private:
+    uint64_t                                _NextSID;
+    std::mutex                              _Mutex;
+    std::unordered_map<uint64_t, SessionPtr> _SessionMap;
+    wsserver_t                              *_Server;
+
+public:
+    SessionManage(wsserver_t *server)
+        : _NextSID(0),
+          _Server(server)
+    { 
+        Log::LogInit("log.txt", NORMAL);
+        Log::LogMessage(INFO, "SessionManage init success");
+    }
+
+    ~SessionManage()
+    {
+        Log::LogMessage(INFO, "SessionManage destroy");
+    }
+
+    SessionPtr CreateSession(uint64_t uid, SESSION_STATE state)
+    {
+        std::lock_guard<std::mutex> lock(_Mutex);
+        SessionPtr session = std::make_shared<Session>(uid, _NextSID);
+        session->SetState(state);
+        _SessionMap.insert(std::make_pair(_NextSID, session));
+        ++_NextSID;
+
+        return session;
+    }
+
+    void AppendSession(const SessionPtr &session)
+    {
+        std::lock_guard<std::mutex> lock(_Mutex);
+        _SessionMap.insert(std::make_pair(session->GetSessionID(), session));
+    }
+
+    SessionPtr GetSessionBySID(uint64_t sid)
+    {
+        std::lock_guard<std::mutex> lock(_Mutex);
+        auto iter = _SessionMap.find(sid);
+        if(iter == _SessionMap.end())
+        {
+            return nullptr;
+        }
+        return iter->second;
+    }
+
+    void RemoveSession(uint64_t sid)
+    {
+        std::lock_guard<std::mutex> lock(_Mutex);
+        auto iter = _SessionMap.find(sid);
+        if(iter == _SessionMap.end())
+        {
+            return;
+        }
+        _SessionMap.erase(iter);
+    }
+
+
+    // 情形 1：如果定时器为空且 ms 为 SESSION_FOREVER，表示会话应该永久存在，直接返回。
+    // 情形 2：如果定时器为空且 ms 不是 SESSION_FOREVER，则设置一个新的定时器，用于在指定时间后删除会话。
+    // 情形 3：如果定时器不为空且 ms 为 SESSION_FOREVER，则取消当前定时器，并将会话设置为永久存在，且将会话的定时器设置为空。
+    // 情形 4：如果定时器不为空且 ms 不是 SESSION_FOREVER，取消当前定时器，并设置会话的新定时器以重置过期时间。
+
+    // Bind Function 
+    // TODO
+    void SetSessionExpireTime(uint64_t sid, int time)
+    {
+        SessionPtr session = GetSessionBySID(sid);
+        if(session.get() == nullptr)
+        {
+            return;
+        }
+        // 获取定时器
+        wsserver_t::timer_ptr timer = session->GetTimer();
+        if(timer == nullptr && time == SESSION_FOREVER)
+        {
+            return;
+        }
+        else if(timer == nullptr && time == SESSION_TIMEOUT)
+        {
+            wsserver_t::timer_ptr new_timer = _Server->set_timer(time, std::bind(&SessionManage::RemoveSession, this, sid));
+            session->SetTimer(new_timer);
+        }
+        else if(timer != nullptr && time == SESSION_FOREVER)
+        {
+            timer->cancel();
+            session->SetTimer(nullptr);
+
+        }
+        else if(timer != nullptr && time == SESSION_FOREVER)
+        {
+            timer->cancel();
+            wsserver_t::timer_ptr new_timer = _Server->set_timer(time, std::bind(&SessionManage::RemoveSession, this, sid));
+            session->SetTimer(new_timer);
+            _Server->set_timer(0, std::bind(&SessionManage::AppendSession, this, session));
+        }
+        else if(timer != nullptr && time == SESSION_TIMEOUT)
+        {
+            // TODO
+            timer->cancel();
+            session->SetTimer(wsserver_t::timer_ptr());
+            _Server->set_timer(0, std::bind(&SessionManage::AppendSession, this, session));
+
+            wsserver_t::timer_ptr new_timer = _Server->set_timer(time, std::bind(&SessionManage::RemoveSession, this, sid));
+            session->SetTimer(new_timer);
+        }
+
+    }
+};
+
+#endif // _SESSION_MANAGE_HPP_
